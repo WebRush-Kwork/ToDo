558d1be7a1adde735fc2884b41cff9525cf715b9
WebRuw <121894121+WebRuw@users.noreply.github.com>
2023-07-17 06:09:38 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const inputTitle = document.querySelector('#title')
 const createBtn = document.querySelector('#create')
 const listElement = document.querySelector('#list')
-
+const openPopupButton = document.querySelector('#open-popup')
+const popupMenu = document.querySelector('#popup-menu')
+const closeButton = document.querySelector('.close')
+const changeTitle = document.querySelector('#change')
+const closePopup = document.querySelector('.close-popup')
+const alertNote = document.querySelector('.alert')
 const tasks = []
 
 const toggleTask = e => {
@@ -10,10 +15,14 @@ const toggleTask = e => {
 		const type = e.target.dataset.type
 		if (type === 'toggle') {
 			tasks[index].isCompleted = !tasks[index].isCompleted
-		} else {
+		} else if (type === 'remove') {
 			tasks.splice(index, 1)
+		} else {
+			changeTitle.value = tasks[index].title
+			changeTitle.dataset.index = index
+			popupMenu.style.display = 'block'
+			alertNote.classList.remove('active')
 		}
-
 		render()
 	}
 }
@@ -23,12 +32,13 @@ const getTemplate = (task, index) => {
   <li class="list-group-item d-flex justify-content-between align-items-center">
     <span class="${task.isCompleted ? 'text-decoration-line-through' : ''}">${
 		task.title
-	}</span>
+	} </span>
     <span>
       <span class="btn btn-small btn-${
 				task.isCompleted ? 'warning' : 'success'
-			}" data-index="${index}" data-type="toggle">&check;</span>
-      <span class="btn btn-small btn-danger" data-index="${index}" data-type="remove">&times;</span>
+			}" w-30 data-index="${index}" data-type="toggle">&check;</span>
+      <span class="btn btn-small btn-danger w-30" data-index="${index}" data-type="remove">&times;</span>
+			<span class="btn btn-small btn-dark w-30" data-index="${index}" data-type="change">\u{270F}</span>
     </span>
   </li>
   `
@@ -55,10 +65,21 @@ const createNewElement = () => {
 		isCompleted: false,
 	}
 	tasks.push(newTask)
-	render()
 	inputTitle.value = ''
+	alertNote.classList.remove('active')
+	render()
 }
 
+const changeTitleFunc = () => {
+	const index = changeTitle.dataset.index
+	tasks[index].title = changeTitle.value
+	alertNote.classList.add('active')
+	popupMenu.style.display = 'none'
+	render()
+}
+
+closeButton.addEventListener('click', () => (popupMenu.style.display = 'none'))
+closePopup.addEventListener('click', changeTitleFunc)
 createBtn.addEventListener('click', createNewElement)
 listElement.addEventListener('click', toggleTask)
 render()
